7268f0e9b853b670bcdacaa034316724de24143d
gabrieldoprado <gabriel@xpcode.com.br>
2018-11-30 00:04:54 -0200

    Marvel

---
+++ Marvel.IoC/Resolver.cs
@@ -0,0 +1,33 @@
+﻿using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marvel.IoC
+{
+    public class Resolver
+    {
+        private readonly Container Container;
+
+        public Resolver()
+        {
+            Container = new Container();
+            RegisterContainers();
+            Container.Verify();
+        }
+
+        private void RegisterContainers()
+        {
+            //Register Domains
+            //Container.Register<ITeste, Teste>();
+        }
+
+        public T Get<T>() where T : class
+        {
+            if (Container == null) throw new InvalidOperationException("Cannot resolve dependencies before the container has been initialized.");
+            return Container.GetInstance<T>();
+        }
+    }
+}
+}
